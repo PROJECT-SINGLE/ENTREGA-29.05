bea06455833da7013eb3f46dc14a620e8a9bab1c
Eryka Lima <erykalima.contato@gmail.com>
2026-06-12 22:51:15 -0300

    Teste Conexão SSH
    Realizada teste de conexão!! Com comando de Script -- Verificando o DB correto

--- TheColor/src/models/usuarioModel.js
+++ TheColor/src/models/usuarioModel.js
@@ -10,13 +10,15 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa, cpf) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+function cadastrar(nome, email, cpf, senha, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED'
+                ,\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD 
+        está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa, cpf) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}', '${cpf}');
+        INSERT INTO usuario (nome, email, cpf, senha, fk_empresa) VALUES ('${nome}', '${email}', '${cpf}', '${senha}', '${fkEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
